2acb54530bd1a457985ae6ce6d4f07d94331d5f7
Daniel <arctic-crow@web.de>
2018-12-05 09:52:06 +0100

    Shamil Work Merge

---
+++ GearVREnergy/Assets/Scripts/ButtonFunctionality.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonFunctionality : MonoBehaviour {
+
+	public void GameScene(int gameScene) 
+	{
+		SceneManager.LoadScene (gameScene);
+	}
+	
+	public void Quit()
+	{
+		UnityEditor.EditorApplication.isPlaying = false;
+	}
+}
---
+++ GearVREnergy/Assets/Scripts/VRButoon.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class VRButoon : MonoBehaviour {
+
+	public Image bgImage;
+	public Color NormalColor;
+	public Color HighLightColor;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void OnGazeEnter()
+	{
+		bgImage.color = HighLightColor;
+	}
+	public void OnGazeExit()
+	{
+		bgImage.color = 	NormalColor;
+	}
+	public void OnClick()
+	{
+		Debug.Log("Horraay it's working!");
+	}
+}
