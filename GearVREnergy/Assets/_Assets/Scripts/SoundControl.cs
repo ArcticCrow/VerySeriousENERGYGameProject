e0b4daf06e3f8c8c5e7c01a83e60b31ccd50901b
PerrinoMichele <micheleperrino97@gmail.com>
2018-12-30 15:50:51 +0100

    sound implementation 1

---
+++ GearVREnergy/Assets/_Assets/Scripts/SoundControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundControl : MonoBehaviour
+{
+    public static AudioClip Door, Teleportation;
+    static AudioSource audioSrc;
+
+    void Start()
+    {
+        //List of all the audio sources being defined
+        Door = Resources.Load<AudioClip>("Door");
+        Teleportation = Resources.Load<AudioClip>("Teleportation");
+
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    public static void PlaySound(string clip)
+    {
+        switch (clip)
+        {
+            case "Door":
+                audioSrc.PlayOneShot(Door);
+                break;
+        }
+        switch (clip)
+        {
+            case "Teleportation":
+                audioSrc.PlayOneShot(Teleportation);
+                break;
+        }
+        
+    }
+}
