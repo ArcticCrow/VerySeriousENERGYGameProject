3c316ce85566de142786cde7294beacc5dafdf69
shamil1233 <43807064+shamil1233@users.noreply.github.com>
2019-01-01 20:52:54 +0100

    up

---
+++ GearVREnergy/Assets/_Assets/Scripts/Throw.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throw : MonoBehaviour {
+
+	float throwForce = 600;
+	Vector3 objectPos;
+	float distance;
+
+	public bool canHold = true;
+	public GameObject item;
+	public GameObject tempParent;
+	public bool isHolding = false;
+
+
+	void Update()
+	{
+ 
+
+        if(isHolding == true)
+		{
+			item.GetComponent<Rigidbody>().velocity = Vector3.zero;
+			item.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+			item.transform.SetParent(tempParent.transform);
+
+			if(Input.GetMouseButtonDown(1))
+			{
+				item.GetComponent<Rigidbody>().AddForce(tempParent.transform.forward * throwForce);
+			}
+		}
+		else
+		{
+			objectPos = item.transform.position;
+			item.transform.SetParent(null);
+			item.GetComponent<Rigidbody>().useGravity = true;
+			item.transform.position = objectPos;
+
+		}
+	}
+
+	void OnMouseDown()
+	{
+		isHolding = true;
+		item.GetComponent<Rigidbody>().useGravity = false;
+		item.GetComponent<Rigidbody>().detectCollisions = false;
+		
+	}
+
+	void OnMouseUp()
+	{
+		isHolding = false;
+	}
+
+}
